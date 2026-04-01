0d69d21082bb36695b62b88404c12693c6bdd1e8
Nathan Blair <me@nathanblair.rocks>
2026-04-01 01:09:57 -0400

    fix: deduplicate tenant attribute

--- internal/source/process.go
+++ internal/source/process.go
@@ -20,11 +20,11 @@ func fail(
 	ctx context.Context,
 	log *slog.Logger,
 	span trace.Span,
-	namespace, msg string,
+	msg string,
 	err error,
 	attrs ...any,
 ) {
-	args := []any{"tenant", namespace, "error", err}
+	args := []any{"error", err}
 	args = append(args, attrs...)
 	log.ErrorContext(ctx, msg, args...)
 	span.RecordError(err)
@@ -53,37 +53,37 @@ func (t *Target) Process(
 
 	tenantMember, err := baggage.NewMemberRaw(tracing.TenantAttribute, namespace)
 	if err != nil {
-		fail(ctx, log, span, namespace, "baggage member failed", err, tracing.TenantAttribute, namespace)
+		fail(ctx, log, span, "baggage member failed", err, tracing.TenantAttribute, namespace)
 		return
 	}
 
 	nameMember, err := baggage.NewMemberRaw(tracing.SourceTargetNameAttribute, t.Name)
 	if err != nil {
-		fail(ctx, log, span, namespace, "baggage member failed", err, tracing.SourceTargetNameAttribute, t.Name)
+		fail(ctx, log, span, "baggage member failed", err, tracing.SourceTargetNameAttribute, t.Name)
 		return
 	}
 
 	repoMember, err := baggage.NewMemberRaw(tracing.SourceTargetRepoURLAttribute, t.RepoURL)
 	if err != nil {
-		fail(ctx, log, span, namespace, "baggage member failed", err, tracing.SourceTargetRepoURLAttribute, t.RepoURL)
+		fail(ctx, log, span, "baggage member failed", err, tracing.SourceTargetRepoURLAttribute, t.RepoURL)
 		return
 	}
 
 	refMember, err := baggage.NewMemberRaw(tracing.SourceTargetRefAttribute, t.Ref)
 	if err != nil {
-		fail(ctx, log, span, namespace, "baggage member failed", err, tracing.SourceTargetRefAttribute, t.Ref)
+		fail(ctx, log, span, "baggage member failed", err, tracing.SourceTargetRefAttribute, t.Ref)
 		return
 	}
 
 	pathMember, err := baggage.NewMemberRaw(tracing.SourceTargetPathAttribute, t.Path)
 	if err != nil {
-		fail(ctx, log, span, namespace, "baggage member failed", err, tracing.SourceTargetPathAttribute, t.Path)
+		fail(ctx, log, span, "baggage member failed", err, tracing.SourceTargetPathAttribute, t.Path)
 		return
 	}
 
 	bag, err := baggage.New(tenantMember, nameMember, repoMember, refMember, pathMember)
 	if err != nil {
-		fail(ctx, log, span, namespace, "baggage creation failed", err)
+		fail(ctx, log, span, "baggage creation failed", err)
 		return
 	}
 
@@ -93,7 +93,7 @@ func (t *Target) Process(
 
 	log.DebugContext(ctx, "acquiring lease")
 	if err = acquireLease(ctx, namespace, t.Name, leaseID, replayCount); err != nil {
-		fail(ctx, log, span, namespace, "lease acquisition failed", err)
+		fail(ctx, log, span, "lease acquisition failed", err)
 		return
 	}
 	log.DebugContext(ctx, "lease acquired")
@@ -101,7 +101,7 @@ func (t *Target) Process(
 	log.DebugContext(ctx, "resolving credentials")
 	authMethod, err := resolveAuth(ctx, namespace, t.CredentialSecret)
 	if err != nil {
-		fail(ctx, log, span, namespace, "authentication failed", err)
+		fail(ctx, log, span, "authentication failed", err)
 		return
 	}
 	log.DebugContext(ctx, "credentials resolved")
@@ -109,7 +109,7 @@ func (t *Target) Process(
 	log.DebugContext(ctx, "cloning source")
 	fs, err := clone(ctx, t.RepoURL, t.Ref, authMethod)
 	if err != nil {
-		fail(ctx, log, span, namespace, "clone failed", err)
+		fail(ctx, log, span, "clone failed", err)
 		return
 	}
 	log.DebugContext(ctx, "source cloned")
@@ -120,7 +120,7 @@ func (t *Target) Process(
 	log.DebugContext(ctx, "verifying lease")
 	holds, err := verifyLease(ctx, namespace, t.Name, leaseID)
 	if err != nil {
-		fail(ctx, log, span, namespace, "lease check failed", err)
+		fail(ctx, log, span, "lease check failed", err)
 		return
 	}
 	log.DebugContext(ctx, "lease verified")
@@ -134,7 +134,7 @@ func (t *Target) Process(
 
 	log.DebugContext(ctx, "streaming to renderer")
 	if err = stream(ctx, fs, t.Path, rendererType); err != nil {
-		fail(ctx, log, span, namespace, "streaming to renderer failed", err)
+		fail(ctx, log, span, "streaming to renderer failed", err)
 		return
 	}
 	log.InfoContext(ctx, "source streamed to renderer")
